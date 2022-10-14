f062496ca07a6d46df01889f34c67c8e0babcc40
kirillanpilov <anpilov.kirill2002@gmail.com>
2022-10-14 16:31:38 +0300

    Add new nice function

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def print_hi(name):
    # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
+def Boo():
+    print("Boo")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
